b8d03e6b334ff784b818d8f6af140d14d6870e4c
ads169573 <54137147+ads169573@users.noreply.github.com>
2024-03-18 21:36:26 +0800

    Update 1914E.cpp
    0318

--- 1914E.cpp
+++ 1914E.cpp
@@ -17,35 +17,22 @@ int main(){
         vector<int> a(n);
         vector<int> b(n);
         PQ<PINT>aa;
-        PQ<PINT>bb;
         for(auto &e:a) cin>>e;
         for(auto &e:b) cin>>e;
         for(int i=0;i<n;++i){
             aa.push({a[i]+b[i],i});
-            bb.push({a[i]+b[i],i});
         }
-        vector<bool> v(n,0);
+
         LL ans=0;
         for(int i=0;i<n;++i){
-            if((i%2)==0){
-                while((!aa.empty())&&v[aa.top().second]){
-                    aa.pop();
-                }
-                v[aa.top().second]=1;
-                
-                ans+=a[aa.top().second]-1;
-                aa.pop();
+            int idx=aa.top().second;
+            aa.pop();
+            if((i%2)==0{
+                ans+=a[idx]-1;
             }
             else{
-                while((!bb.empty())&&v[bb.top().second]){
-                    bb.pop();
-                }
-                v[bb.top().second]=1;
-                
-                ans-=b[bb.top().second]-1;
-                bb.pop();
+                ans-=b[idx]-1;
             }
-            
         }
         cout<<ans<<endl;
 	}
